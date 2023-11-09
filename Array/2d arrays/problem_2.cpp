5a38949b35cf3d462667f2b11506f34145f62957
Mihirpatel6996 <96419193+Mihirpatel6996@users.noreply.github.com>
2023-11-09 23:27:47 +0530

    Add files via upload

---
+++ Array/2d arrays/problem_2.cpp
@@ -0,0 +1,63 @@
+//  spiral printing in a 2-d array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int m, n, key;
+    cout << "enter array size";
+    cin >> n >> m;
+
+    //  2-d array initialization
+
+    int arr[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+
+            cin >> arr[i][j];
+        }
+    }
+
+    int row_start = 0, row_end = n - 1, col_start = 0, col_end = m - 1;
+
+    while (row_start <= row_end && col_start <= col_end)
+    {
+
+        // for row start
+
+        for (int col = col_start; col <= col_end; col++)
+        {
+            cout << arr[row_start][col] << " ";
+        }
+        row_start++;
+
+        // for col start
+        for (int row = row_start; row <= row_end; row++)
+        {
+            cout << arr[row][col_end] << " ";
+        }
+        col_end--;
+
+        // for row wnd
+
+        for (int col = col_end; col >= col_start; col--)
+        {
+            cout << arr[row_end][col] << " ";
+        }
+        row_end--;
+
+        // for col start
+
+        for (int row = row_end; row >= row_start; row--)
+        {
+            cout << arr[row][col_start] << " ";
+        }
+        col_start++;
+    }
+    return 0;
+}
